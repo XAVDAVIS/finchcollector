c419e868e94d6f3f1dd37fd19843e49e5fc4aa1b
Xavier <shottyx5156@hotmail.com>
2023-02-06 09:31:13 -0500

    running in browser

---
+++ main_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
